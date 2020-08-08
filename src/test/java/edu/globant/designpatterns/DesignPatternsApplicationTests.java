808e2ee68aac49129e68dde24dad05d8e71a9960
Rodrigo Diaz <esteban.diaz@globant.com>
2020-08-08 17:09:05 -0400

    Base code

---
+++ src/test/java/edu/globant/designpatterns/DesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.globant.designpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
